a860954eaa74d0a31745c23af854408575960936
Theron Halsey <theron.halsey@gmail.com>
2020-07-17 13:34:02 -0400

    basic function

--- script.js
+++ script.js
@@ -6,14 +6,43 @@ const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
+let numClosedDoors = 3;
+
+let openDoor1 = null;
+let openDoor2 = null;
+let openDoor3 = null;
+
+const randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numClosedDoors);
+  switch (true) {
+    case (choreDoor === 0):
+      openDoor1 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    case (choreDoor === 1):
+      openDoor1 = beachDoorPath;
+      openDoor2 = botDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    case (choreDoor === 2):
+      openDoor1 = beachDoorPath;
+      openDoor2 = spaceDoorPath;
+      openDoor3 = botDoorPath;
+      break;
+  };
+};
+
 door1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 };
 
 door2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 };
 
 door3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 };
+
+randomChoreDoorGenerator();
